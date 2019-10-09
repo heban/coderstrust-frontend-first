caff372fc1678c75274871ca80e69fcda87b104f
Paweł Ochota <pawel.ochota@start-up.house>
2019-10-09 09:02:45 +0200

    Updates

---
+++ precache-manifest.1785bdb2c842a37d0ba432a33087cdd5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "de9f2df89dc78b7fdf4aee946e34c626",
+    "url": "/coderstrust-frontend-first/static/media/es.de9f2df8.gif"
+  },
+  {
+    "revision": "09b3d6d33daba6a460de",
+    "url": "/coderstrust-frontend-first/static/js/main.09b3d6d3.chunk.js"
+  },
+  {
+    "revision": "cfffe8eaa0277e8f7abf2fd4af32b306",
+    "url": "/coderstrust-frontend-first/static/media/tony.cfffe8ea.gif"
+  },
+  {
+    "revision": "fc0d5c9ec19402ce63ed",
+    "url": "/coderstrust-frontend-first/static/js/runtime~main.fc0d5c9e.js"
+  },
+  {
+    "revision": "d458b7bb54beda49eadcde47d0fafa4a",
+    "url": "/coderstrust-frontend-first/static/media/js-intro.d458b7bb.jpg"
+  },
+  {
+    "revision": "5f7e538fa13a0668fa4d10db75e7b13e",
+    "url": "/coderstrust-frontend-first/static/media/pablo.5f7e538f.jpg"
+  },
+  {
+    "revision": "de27d7c2580294781e753a498bda6517",
+    "url": "/coderstrust-frontend-first/static/media/letsrock.de27d7c2.gif"
+  },
+  {
+    "revision": "1c948286ea5859a72015a746abd5a09f",
+    "url": "/coderstrust-frontend-first/static/media/first-page.1c948286.jpg"
+  },
+  {
+    "revision": "7e46d10de125b747feefd326cee6299e",
+    "url": "/coderstrust-frontend-first/static/media/js-intro2.7e46d10d.jpg"
+  },
+  {
+    "revision": "7f48e9ce1d7f556464a55ce02d5c7675",
+    "url": "/coderstrust-frontend-first/static/media/bag-intro.7f48e9ce.png"
+  },
+  {
+    "revision": "52aa0ab1a62b3edf3c3179c9eac5c30a",
+    "url": "/coderstrust-frontend-first/static/media/bag3.52aa0ab1.png"
+  },
+  {
+    "revision": "a8de9701466c8148668d44a627673598",
+    "url": "/coderstrust-frontend-first/static/media/deepbreath.a8de9701.webp"
+  },
+  {
+    "revision": "1e4a49d10b5cced0402079ca7d523014",
+    "url": "/coderstrust-frontend-first/static/media/omg.1e4a49d1.webp"
+  },
+  {
+    "revision": "ff362d5d8d8fc74030da5f6a80f2355f",
+    "url": "/coderstrust-frontend-first/static/media/more.ff362d5d.gif"
+  },
+  {
+    "revision": "b865ffc60a81d392da91",
+    "url": "/coderstrust-frontend-first/static/js/1.b865ffc6.chunk.js"
+  },
+  {
+    "revision": "b865ffc60a81d392da91",
+    "url": "/coderstrust-frontend-first/static/css/1.6df55228.chunk.css"
+  },
+  {
+    "revision": "a7d75afa3d6a2a7295ce20e10dd1693c",
+    "url": "/coderstrust-frontend-first/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/coderstrust-frontend-first/precache-manifest.1785bdb2c842a37d0ba432a33087cdd5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/coderstrust-frontend-first/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
