bbfda9e298c40d65da21c642d3d588af291a89b6
Paweł Ochota <pawel.ochota@start-up.house>
2019-10-09 08:55:42 +0200

    Add prod build

---
+++ build/precache-manifest.9cdabbf7d09e1d9899e953994dbbe351.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "de9f2df89dc78b7fdf4aee946e34c626",
+    "url": "/static/media/es.de9f2df8.gif"
+  },
+  {
+    "revision": "09b3d6d33daba6a460de",
+    "url": "/static/js/main.09b3d6d3.chunk.js"
+  },
+  {
+    "revision": "cfffe8eaa0277e8f7abf2fd4af32b306",
+    "url": "/static/media/tony.cfffe8ea.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d458b7bb54beda49eadcde47d0fafa4a",
+    "url": "/static/media/js-intro.d458b7bb.jpg"
+  },
+  {
+    "revision": "5f7e538fa13a0668fa4d10db75e7b13e",
+    "url": "/static/media/pablo.5f7e538f.jpg"
+  },
+  {
+    "revision": "de27d7c2580294781e753a498bda6517",
+    "url": "/static/media/letsrock.de27d7c2.gif"
+  },
+  {
+    "revision": "1c948286ea5859a72015a746abd5a09f",
+    "url": "/static/media/first-page.1c948286.jpg"
+  },
+  {
+    "revision": "7e46d10de125b747feefd326cee6299e",
+    "url": "/static/media/js-intro2.7e46d10d.jpg"
+  },
+  {
+    "revision": "7f48e9ce1d7f556464a55ce02d5c7675",
+    "url": "/static/media/bag-intro.7f48e9ce.png"
+  },
+  {
+    "revision": "52aa0ab1a62b3edf3c3179c9eac5c30a",
+    "url": "/static/media/bag3.52aa0ab1.png"
+  },
+  {
+    "revision": "a8de9701466c8148668d44a627673598",
+    "url": "/static/media/deepbreath.a8de9701.webp"
+  },
+  {
+    "revision": "1e4a49d10b5cced0402079ca7d523014",
+    "url": "/static/media/omg.1e4a49d1.webp"
+  },
+  {
+    "revision": "ff362d5d8d8fc74030da5f6a80f2355f",
+    "url": "/static/media/more.ff362d5d.gif"
+  },
+  {
+    "revision": "b865ffc60a81d392da91",
+    "url": "/static/js/1.b865ffc6.chunk.js"
+  },
+  {
+    "revision": "b865ffc60a81d392da91",
+    "url": "/static/css/1.6df55228.chunk.css"
+  },
+  {
+    "revision": "0d65dc68f4e2861dda9ec07135f70021",
+    "url": "/index.html"
+  }
+];
